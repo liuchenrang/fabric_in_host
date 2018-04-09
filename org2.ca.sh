df45041f6e94e22c5cf5eb90fd0e6f44017dc946
xinghuo <xinghuo@In66.com>
2018-04-09 16:15:22 +0800

    update

---
+++ org2.ca.sh
@@ -0,0 +1,14 @@
+source base.sh
+# the following setting starts chaincode containers on the same
+# bridge network as the peers
+# https://docs.docker.com/compose/networking/
+export FABRIC_CA_HOME=$CONFIG_TX_PATH/crypto-config/peerOrganizations/org2.example.com/ca
+CA1_PRIVATE_KEY=`ls -l $FABRIC_CA_HOME |grep sk|awk '{print $9}'`
+export FABRIC_CA_SERVER_CA_NAME=ca-org2
+export FABRIC_CA_SERVER_TLS_ENABLED=true
+export FABRIC_CA_SERVER_ADDRESS=0.0.0.0
+export FABRIC_CA_SERVER_PORT=8054
+export FABRIC_CA_SERVER_TLS_CERTFILE=$FABRIC_CA_HOME/ca.org2.example.com-cert.pem
+export FABRIC_CA_SERVER_TLS_KEYFILE=$FABRIC_CA_HOME/$CA1_PRIVATE_KEY
+
+fabric-ca-server start  -b admin:adminpw -d
