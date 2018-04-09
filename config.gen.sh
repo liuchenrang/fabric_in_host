16cb86a0d067951ad0f48c1dd5898f0e3bd076ec
xinghuo <xinghuo@In66.com>
2018-04-09 18:52:46 +0800

    update

--- config.gen.sh
+++ config.gen.sh
@@ -1,6 +1,13 @@
-rm -rf channel-artifacts
-rm -rf crypto-config
+#!/bin/bash
+source clear.sh
+CHANNEL_NAME=mychannel
 mkdir channel-artifacts
 cryptogen generate --config=./crypto-config.yaml
-configtxgen -profile TwoOrgsChannel -outputCreateChannelTx ./channel-artifacts/channel.tx -channelID  mychannel
+configtxgen -profile TwoOrgsChannel -outputCreateChannelTx ./channel-artifacts/channel.tx -channelID  $CHANNEL_NAME
 configtxgen -profile TwoOrgsOrdererGenesis -outputBlock ./channel-artifacts/genesis.block
+  
+  
+configtxgen -profile TwoOrgsChannel -outputAnchorPeersUpdate ./channel-artifacts/Org1MSPanchors.tx -channelID $CHANNEL_NAME -asOrg Org1MSP
+configtxgen -profile TwoOrgsChannel -outputAnchorPeersUpdate ./channel-artifacts/Org2MSPanchors.tx -channelID $CHANNEL_NAME -asOrg Org2MSP
+
+source create.channel.sh
--- join.sh
+++ join.sh
@@ -1,7 +1,28 @@
 #!/bin/bash
-source org1.peer0.env.sh
+source base.sh
+# export CORE_PEER_LOCALMSPID="Org1MSP"
+export CORE_PEER_TLS_ROOTCERT_FILE=$CONFIG_TX_PATH/crypto-config/peerOrganizations/org1.example.com/peers/peer0.org1.example.com/tls/ca.crt
 export CORE_PEER_MSPCONFIGPATH=$CONFIG_TX_PATH/crypto-config/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp
-peer $@
+export CORE_PEER_ADDRESS=peer0.org1.example.com:7051
+peer channel join  -o orderer.example.com:7050 --tls true -b mychannel.block
 
-#channel  create -c mychannel -f channel-artifacts/channel.tx --cafile ./crypto-config/ordererOrganizations/example.com/tlsca/tlsca.example.com-cert.pem -o orderer.example.com --tls true
-#sh ./join.sh channel join --cafile crypto-config/ordererOrganizations/example.com/tlsca/tlsca.example.com-cert.pem -o orderer.example.com:7050 --tls true -b mychannel.block
+
+export CORE_PEER_LOCALMSPID="Org1MSP"
+export CORE_PEER_TLS_ROOTCERT_FILE=$CONFIG_TX_PATH/crypto-config/peerOrganizations/org1.example.com/peers/peer0.org1.example.com/tls/ca.crt
+export CORE_PEER_MSPCONFIGPATH=$CONFIG_TX_PATH/crypto-config/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp
+export CORE_PEER_ADDRESS=peer1.org1.example.com:8051
+peer channel join --cafile crypto-config/ordererOrganizations/example.com/tlsca/tlsca.example.com-cert.pem -o orderer.example.com:7050 --tls true -b mychannel.block
+
+
+export CORE_PEER_LOCALMSPID="Org2MSP"
+export CORE_PEER_TLS_ROOTCERT_FILE=$CONFIG_TX_PATH/crypto-config/peerOrganizations/org2.example.com/peers/peer0.org2.example.com/tls/ca.crt
+export CORE_PEER_MSPCONFIGPATH=$CONFIG_TX_PATH/crypto-config/peerOrganizations/org2.example.com/users/Admin@org2.example.com/msp
+export CORE_PEER_ADDRESS=peer0.org2.example.com:9051
+peer channel join --cafile crypto-config/ordererOrganizations/example.com/tlsca/tlsca.example.com-cert.pem -o orderer.example.com:7050 --tls true -b mychannel.block
+
+
+export CORE_PEER_LOCALMSPID="Org2MSP"
+export CORE_PEER_TLS_ROOTCERT_FILE=$CONFIG_TX_PATH/crypto-config/peerOrganizations/org2.example.com/peers/peer1.org2.example.com/tls/ca.crt
+export CORE_PEER_MSPCONFIGPATH=$CONFIG_TX_PATH/crypto-config/peerOrganizations/org2.example.com/users/Admin@org2.example.com/msp
+export CORE_PEER_ADDRESS=peer1.org2.example.com:10051
+peer channel join --cafile crypto-config/ordererOrganizations/example.com/tlsca/tlsca.example.com-cert.pem -o orderer.example.com:7050 --tls true -b mychannel.block
