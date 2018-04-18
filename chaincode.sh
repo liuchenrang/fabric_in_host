19191e57fcd24d76a7d46f7d2ffe803e0cbdb969
xinghuo <xinghuo@In66.com>
2018-04-18 16:59:44 +0800

    update

--- chaincode.sh
+++ chaincode.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
+source base.sh
 cp -rf chaincode $GOPATH/src
 # 将first-fabric 的 chaincode 拷贝到 $GOPATH/src 下
 bash ./cli.sh  chaincode install -n fabcar -v 1.0 -p chaincode/fabcar
 
-bash ./cli.sh chaincode instantiate --cafile crypto-config/ordererOrganizations/example.com/tlsca/tlsca.example.com-cert.pem  --tls true  -o orderer.example.com:7050 -C mychannel -n fabcar -v 1.0 -c '{"Args":[""]}' 
+bash ./cli.sh chaincode instantiate --cafile $CONFIG_TX_PATH/crypto-config/ordererOrganizations/example.com/tlsca/tlsca.example.com-cert.pem  --tls true  -o orderer.example.com:7050 -C mychannel -n fabcar -v 1.0 -c '{"Args":[""]}' 
 # -P "OR ('Org1MSP.member','Org2MSP.member')"
 # wait instantiate docker 
 sleep 30 
-bash ./cli.sh chaincode invoke -o orderer.example.com:7050 --cafile crypto-config/ordererOrganizations/example.com/tlsca/tlsca.example.com-cert.pem  --tls true  -C mychannel -n fabcar -c '{"function":"initLedger","Args":[""]}'
+bash ./cli.sh chaincode invoke -o orderer.example.com:7050 --cafile $CONFIG_TX_PATH/crypto-config/ordererOrganizations/example.com/tlsca/tlsca.example.com-cert.pem  --tls true  -C mychannel -n fabcar -c '{"function":"initLedger","Args":[""]}'
 
