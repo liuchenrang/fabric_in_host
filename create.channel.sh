31f8f16e36aaab010ba6ea29b918fc118584d074
xinghuo <xinghuo@In66.com>
2018-04-18 14:13:25 +0800

    update

--- create.channel.sh
+++ create.channel.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 source org1.peer0.env.sh
 export CORE_PEER_MSPCONFIGPATH=$CONFIG_TX_PATH/crypto-config/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp
-peer channel create -c mychannel -f channel-artifacts/channel.tx --cafile $CONFIG_TX_PATH/crypto-config/ordererOrganizations/example.com/tlsca/tlsca.example.com-cert.pem -o orderer.example.com:7050 --tls true
+# peer channel create -c mychannel -f channel-artifacts/channel.tx --cafile $CONFIG_TX_PATH/crypto-config/ordererOrganizations/example.com/tlsca/tlsca.example.com-cert.pem -o orderer.example.com:7050 --tls true
+peer channel create -c xinghuo -f channel-artifacts/channel.tx --cafile $CONFIG_TX_PATH/crypto-config/ordererOrganizations/example.com/tlsca/tlsca.example.com-cert.pem -o orderer.example.com:7050 --tls true
 
--- org1.ca.sh
+++ org1.ca.sh
@@ -2,18 +2,27 @@ source base.sh
 # the following setting starts chaincode containers on the same
 # bridge network as the peers
 # https://docs.docker.com/compose/networking/
+export CONFIG_TX_PATH=`pwd`
+
 export FABRIC_CA_HOME=$CONFIG_TX_PATH/crypto-config/peerOrganizations/org1.example.com/ca
 CA1_PRIVATE_KEY=`ls -l $FABRIC_CA_HOME |grep sk|awk '{print $9}'`
 
-export FABRIC_CA_SERVER_CA_NAME=ca-org1
+export FABRIC_CA_SERVER_CA_NAME=ca.org1.example.com
 export FABRIC_CA_SERVER_TLS_ENABLED=true
 export FABRIC_CA_SERVER_ADDRESS=0.0.0.0
 export FABRIC_CA_SERVER_PORT=7054
 export FABRIC_CA_DEBUG=true
 export FABRIC_CA_SERVER_TLS_CERTFILE=$FABRIC_CA_HOME/ca.org1.example.com-cert.pem
 export FABRIC_CA_SERVER_TLS_KEYFILE=$FABRIC_CA_HOME/$CA1_PRIVATE_KEY
+# export FABRIC_CA_SERVER_DB_TYPE=mysql
+# export FABRIC_CA_SERVER_DB_DATASOURCE=manager:managermanager@tcp\(localhost:3306\)/fabricca?parseTime=true
 
+# export FABRIC_CA_SERVER_DB_TYPE=postgres
+# export FABRIC_CA_SERVER_DB_DATASOURCE=dbname=fabric_ca_server localhost port=5432 user=xinghuo password=1 sslmode=disable
+
+echo $FABRIC_CA_SERVER_TLS_KEYFILE
 # fabric-ca-server $@
 # rm -f $FABRIC_CA_HOME/fabric-ca-server.db
-ls $FABRIC_CA_HOME/fabric-ca-server-config.yaml
+# echo  "$FABRIC_CA_HOME/fabric-ca-server-config.yaml"
 fabric-ca-server   start  --ca.certfile  $FABRIC_CA_SERVER_TLS_CERTFILE --ca.keyfile $FABRIC_CA_SERVER_TLS_KEYFILE -b admin:adminpw -d
+# ls $FABRIC_CA_HOME/ca.org1.example.com-cert.pem
